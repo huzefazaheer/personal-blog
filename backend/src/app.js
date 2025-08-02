39f368cd8bd4e02b06ba60c4f1c398b72f9e1a32
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-08-02 19:56:50 +0500

    Updated cors options

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const corsoptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.ADMINAPP,
   optionsSuccessStatus: 200,
 }
 
